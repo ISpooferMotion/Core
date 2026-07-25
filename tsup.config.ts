e16c60223fc3ce038cb8c9890b4705c5ffa45bca
cody <cflem1214@gmail.com>
2026-07-25 10:20:23 -0400

    fix(build): replace tsup dts generation with tsc to support TS 7+

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: false,
   sourcemap: true,
   target: "es2022",
   clean: true,
